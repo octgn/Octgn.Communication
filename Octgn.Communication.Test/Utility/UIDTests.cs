49842818205a83a4e3856fbcd09154b4b76f31ee
Kelly Elton <its.the.doc@gmail.com>
2018-02-09 20:32:27 -0600

    Test to verify no duplicate id's

---
+++ Octgn.Communication.Test/Utility/UIDTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Octgn.Communication.Utility;
+using System.Collections.Generic;
+
+namespace Octgn.Communication.Test.Utility
+{
+    [TestFixture]
+    public class UIDTests
+    {
+        [TestCase]
+        public void NoDuplicates() {
+            var values = new HashSet<string>();
+            for(var i = 0; i < 5000000; i++) {
+                values.Add(UID.Generate(i));
+            }
+        }
+    }
+}
